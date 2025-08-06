1753b7de06530274005f72434ddb8d1bde5e54db
vincent8494 <fettymikev@gmail.com>
2025-08-06 23:12:21 +0300

    chore: remove unused uploads route for Cloudinary-only deployment

--- app.py
+++ app.py
@@ -55,9 +55,6 @@ def index():
         uploads = []
     return render_template('index.html', uploads=uploads)
 
-@app.route('/uploads/<filename>')
-def uploaded_file(filename):
-    return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
